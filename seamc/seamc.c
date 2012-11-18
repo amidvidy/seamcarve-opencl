9ef50807c853ba940243e1e1712f1d9853f56bad
Erik <ejrogers@berkeley.edu>
2012-11-17 23:03:35 -0800

    Removed python source from comments. Left only name of original python functions.

--- seamc/seamc.c
+++ seamc/seamc.c
@@ -31,17 +31,8 @@ void SEAMC_mk_kernel(float** K)
             pK_y[x] = c0 * exp((xm * xm + ym * ym) * c1);
         }
     }
-} /* SOURCE:
- def mk_kernel(K):
- s = 2.3;
- c0 = 1.0 / (2.0*math.pi*s*s);
- c1 = -1.0 / (2.0*s*s);
- for y in range(0,5):
- ym = y - 2;
- for x in range(0,5):
- xm = x - 2;
- K[y][x] = c0 * math.exp((xm*xm + ym*ym)*c1);
- */
+} // def mk_kernel(K):
+
 
 void SEAMC_tfj_conv2d(SEAMC_WORK_p pWORK, float **I, float **O, float **K)
 {
@@ -60,14 +51,8 @@ void SEAMC_tfj_conv2d(SEAMC_WORK_p pWORK, float **I, float **O, float **K)
             }
         }
     }
-} /* SOURCE:
- def tfj_conv2d(I,O,K):
- for y in range(3,ydim):
- for x in range(3,xdim):
- for yy in range(-2,3):
- for xx in range(-2, 3):
- O[y][x] = O[y][x] + K[2+yy][2+xx] * I[y+yy][x+xx];
- */
+} // def tfj_conv2d(I,O,K):
+
 
 void SEAMC_dp(SEAMC_WORK_p pWORK, float **Y, float **G)
 {
@@ -83,12 +68,8 @@ void SEAMC_dp(SEAMC_WORK_p pWORK, float **Y, float **G)
                     + fmin(fmin(pY_yp[x - 1], pY_yp[x]), pY_yp[x + 1]);
         }
     }
-} /* SOURCE:
- def dp(Y,G):
- for i in range(5,yydim):
- for j in range(5,xxdim):
- Y[i][j] = G[i][j] + min(min(Y[i-1][j-1], Y[i-1][j]),Y[i-1][j+1]);
- */
+} // def dp(Y,G):
+
 
 void SEAMC_copyKernel(SEAMC_WORK_p pWORK, float **I, int width_m1, int32_t *C)
 {
@@ -101,12 +82,8 @@ void SEAMC_copyKernel(SEAMC_WORK_p pWORK, float **I, int width_m1, int32_t *C)
             pI_y[x] = pI_y[x + 1];
         }
     }
-} /* SOURCE:
- def copyKernel(I,width_m1,c):
- for i in range(0,height):
- for j in range(c, width_m1):
- I[i][j] = I[i][j+1]
- */
+} // def copyKernel(I,width_m1,c):
+
 
 void SEAMC_zeroKernel(float **Y, int h, int w)
 {
@@ -118,12 +95,8 @@ void SEAMC_zeroKernel(float **Y, int h, int w)
             pY_y[x] = 0;
         }
     }
-} /* SOURCE:
- def zeroKernel(Y,h,w):
- for i in range(0,h):
- for j in range(0,w):
- Y[i][j] = 0
- */
+} // def zeroKernel(Y,h,w):
+
 
 void SEAMC_padKernel(float **OO, int h, int w)
 {
@@ -137,13 +110,8 @@ void SEAMC_padKernel(float **OO, int h, int w)
             pOO_y[ylast - x] = 1000000.0;
         }
     }
-} /* SOURCE:
- def padKernel(OO,h,w):
- for i in range(0,h):
- for j in range(0,20):
- OO[i][j] = 1000000.0;
- OO[i][w-j-1] = 1000000.0;
- */
+} // def padKernel(OO,h,w):
+
 
 void SEAMC_backtrack(SEAMC_WORK_p pWORK, float **Y, int *O)
 {
@@ -183,43 +151,8 @@ void SEAMC_backtrack(SEAMC_WORK_p pWORK, float **Y, int *O)
         if (idx < 5) idx = 5;
         O[i] = idx;
     }
-} /* SOURCE:
- def backtrack(Y,O):
- idx = 5;
- min_v = 100000000.0;
- 
- for i in range(idx,(width-5)):
- if(Y[yydim-1][i] < min_v):
- min_v = Y[yydim-1][i]
- idx = i
- 
- #print 'idx=%d, min_v=%f' % (idx, min_v)
- O[yydim-1] = idx;
- 
+} // def backtrack(Y,O):
 
- for y in range(2, (yydim-1)):
- i = (yydim - y);
- 
- L = Y[i][idx-1];
- C = Y[i][idx];
- R = Y[i][idx+1];
-
- if(L < C):
- if(L < R):
- idx = idx-1;
- else:
- idx = idx+1;
- else:
- if(C < R):
- idx = idx;
- else:
- idx = idx + 1;
-
- #print 'i=%d,idx=%d' % (i,idx)
- idx = min(idx, (width-5));
- idx = max(idx, 5);
- O[i] = idx;
- */
 
 float** SEAMC_carveGrey(float **iM, int iH, int iW, int newH, int newW)
 { // WARNING: Modifies the input matrix too :)
@@ -300,62 +233,5 @@ float** SEAMC_carveGrey(float **iM, int iH, int iW, int newH, int newW)
         memmove(newM[y], iM[y], newW * sizeof(float));
     }
     return newM;
-} /* SOURCE:
- K = np.ones((5,5), dtype=np.float32); # I think the "ones" is not needed???
- B = np.zeros(height,dtype=np.int32);
+}
 
- mk_kernel(K); # This probably writes to all 5x5 values???
-
- owidth = ???
- oheight = ???
- num_carve = ???;
- disableTFJ = ???;
-
- O = np.zeros((height,width),dtype=np.float32);
- OO = np.zeros((height,width),dtype=np.float32);
-
- while (width > (owidth - num_carve)):
- start = time.time();
- ydim = height-3;
- xdim = width-3;
- yydim = height-5;
- xxdim = width-5;
- 
- zeroKernel(O,oheight,np.int32(width));
- zeroKernel(OO,oheight,np.int32(width));
-
- #using closures here...
- tfj_conv2d(I,O,K);
-
- #pad with 'float max'
- #padKernel(OO,np.int32(height),np.int32(width));
- for i in range(0,height):
- for j in range(0,20):
- OO[i][j] = 1000000.0;
- OO[i][width-j-1] = 1000000.0;
- 
- #do dynamic programming
- dp(OO,O)
-
- #backtrack in python            
- #t0 = time.time();
- backtrack(OO,B)
- #t1 = time.time() - t0;
- #print '%f sec in backtrack (py function)' % t1
-
- for i in range(yydim,height):
- B[i] = B[yydim-1];
- for i in range(0, 2):
- B[i] = B[2];
- 
- copyKernel(I,np.int32((width-1)),B[i]);
-
- width = width - 1;
- elapsed = (time.time() - start);
- print '%f sec per iteration' % elapsed
-
- II = np.zeros((height,(width+1)),dtype=np.float32);
-
- for i in range(0,height):
- II[i] = I[i][:(width+1)]
- */
