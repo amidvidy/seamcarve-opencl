57c48d5dd0507b098c3ecacce1376985cb3c6ec2
amidvidy <amidvidy@gmail.com>
2012-11-26 21:18:23 -0800

    added roundUp

--- seamcl/src/seamcl.cpp
+++ seamcl/src/seamcl.cpp
@@ -223,6 +223,26 @@ namespace image {
 
 } // namespace image {
 
+// Math utils
+namespace math {
+
+    /**
+     * Utility function to round up to the nearest multiple of the group size.
+     * @param groupSize The work group size.
+     * @param globalSize The unrounded globalSize
+     * @return The smallest multiple of groupSize greater than globalSize.
+     */
+    size_t roundUp(int groupSize, int globalSize) {
+        int r = globalSize % groupSize;
+        if (r == 0) {
+            return globalSize;
+        } else {
+            return globalSize + groupSize - r;
+        }
+    }
+
+} // namespace math
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         std::cerr << "USAGE: seamcl <IMAGENAME>" << std::endl;
@@ -260,6 +280,8 @@ int main(int argc, char** argv) {
         std::cerr << "Error setting kernel arguments." << std::endl;
         exit(-1);
     }
+
+    
 
     std::cout << "SUCCESS!" << std::endl;
 
