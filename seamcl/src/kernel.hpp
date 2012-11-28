ce3180a1388643ee6db222a4f0a80f8f4e6fd48c
Jonas Mueller <jonas@ubik.(none)>
2012-11-28 00:36:36 -0800

    wrapper for laplacian added to kernel.hpp

--- seamcl/src/kernel.hpp
+++ seamcl/src/kernel.hpp
@@ -243,8 +243,101 @@ namespace kernel {
         delete [] deviceResult;
         /** END DEBUGGING **/
     }
+    
+    /**
+     * Computes the laplacian of the gaussian convolution
+     *  with an image (using openCL).
+     * @param ctx An openCL context object.
+     * @param cmdQueue An openCL command queue.
+     * @param inputImage The image to blur
+     * @param energyMatrix An openCL buffer to store the output in.
+     * @param sampler An openCL image sampler object.
+     * @param height The height of the input image.
+     * @param width The width of the input image.
+     * @return A buffer containing the gradient interpreted as a matrix of size height * width.
+     */
+    void laplacian(cl::Context &ctx,
+                  cl::CommandQueue &cmdQueue,
+                  cl::Image2D &inputImage,
+                  cl::Buffer &energyMatrix,
+                  cl::Sampler &sampler,
+                  int height,
+                  int width) {
 
 
+        // Setup kernel
+        cl::Kernel kernel = setup::kernel(ctx, std::string("LaplacianGaussianKernel.cl"), std::string("gaussian_laplacian"));
+
+        cl_int errNum;
+
+        // Set kernel arguments
+        errNum = kernel.setArg(0, inputImage);
+        errNum |= kernel.setArg(1, energyMatrix);
+        errNum |= kernel.setArg(2, sampler);
+        errNum |= kernel.setArg(3, width);
+        errNum |= kernel.setArg(4, height);
+
+        if (errNum != CL_SUCCESS) {
+            std::cerr << "Error setting laplacian kernel arguments." << std::endl;
+            exit(-1);
+        }
+
+        cl::NDRange offset = cl::NDRange(0, 0);
+        cl::NDRange localWorkSize = cl::NDRange(16, 16);
+        cl::NDRange globalWorkSize = cl::NDRange(math::roundUp(localWorkSize[0], width),
+                                                 math::roundUp(localWorkSize[1], height));
+
+        errNum = cmdQueue.enqueueNDRangeKernel(kernel,
+                                               offset,
+                                               globalWorkSize,
+                                               localWorkSize);
+
+        if (errNum != CL_SUCCESS) {
+            std::cerr << "Error enqueuing laplacian kernel for execution." << std::endl;
+            exit(-1);
+        }
+
+        // TODO(amidvidy): make this debugging code
+        // Read data into an image object
+        cl::Image2D gradientImage = cl::Image2D(ctx,
+                                                (cl_mem_flags) CL_MEM_READ_WRITE,
+                                                cl::ImageFormat(CL_LUMINANCE, CL_FLOAT),
+                                                width,
+                                                height,
+                                                0,
+                                                NULL,
+                                                &errNum);
+        if (errNum != CL_SUCCESS) {
+            std::cerr << "Error creating laplacian output image" << std::endl;
+            exit(-1);
+        }
+
+        cl::size_t<3> origin;
+        origin.push_back(0);
+        origin.push_back(0);
+        origin.push_back(0);
+        cl::size_t<3> region;
+        region.push_back(width);
+        region.push_back(height);
+        region.push_back(1);
+
+        errNum = cmdQueue.enqueueCopyBufferToImage(energyMatrix,
+                                                   gradientImage,
+                                                   0,
+                                                   origin,
+                                                   region,
+                                                   NULL,
+                                                   NULL);
+
+        if (errNum != CL_SUCCESS) {
+            std::cerr << "Error copying laplacian image from buffer" << std::endl;
+            std::cerr << "ERROR_CODE = " << errNum << std::endl;
+        }
+
+        image::save(cmdQueue, gradientImage, std::string("gradient_output.tif"), height, width);
+        /** END DEBUGGING */
+
+    } // End of laplacian method.
 
 } // namespace kernel {
 
