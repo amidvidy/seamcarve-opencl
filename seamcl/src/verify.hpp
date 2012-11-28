2ab3489ce0efa63b16306150b50d0a0424efba60
amidvidy <amidvidy@gmail.com>
2012-11-27 23:04:01 -0800

    still debugging verify

--- seamcl/src/verify.hpp
+++ seamcl/src/verify.hpp
@@ -5,6 +5,7 @@
 #include <cmath>
 
 // STL
+#include <iostream>
 #include <algorithm>
 
 // OpencL
@@ -22,9 +23,11 @@ namespace verify {
                       int height,
                       int pitch) {
 
-        float hostResult[width * height];
+        std::cerr << "in verify::computeSeams" << std::endl;
+        float *hostResult = new float[width * height];
+        std::cerr << "Before memset" << std::endl;
         memset(hostResult, 0, width * height * sizeof(float));
-
+        std::cerr << "After memset" << std::endl;
         for (int x = inset; x < width - inset; ++x) {
             for (int y = 1; y < height; ++y) {
                 rM(hostResult, x, y) = rM(originalEnergyMatrix, x, y) + std::min(rM(hostResult, x, y-1),
@@ -37,10 +40,12 @@ namespace verify {
         for (int x = 0; x < width; ++x) {
             for (int y = 0; y < height; ++y) {
                 if (fabsf(rM(hostResult, x, y) - rM(deviceResult, x, y)) > epsilon) {
+                    delete [] hostResult;
                     return false;
                 }
             }
         }
+        delete [] hostResult;
         return true;
     }
 
