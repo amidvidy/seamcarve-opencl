18d823a428f3a0e1ea6b6a5cf875f4730019c943
Erik <ejrogers@berkeley.edu>
2012-11-29 10:47:58 -0800

    timing scripts

---
+++ castles/time.sh
@@ -0,0 +1,20 @@
+echo "time ~/seamc castle-10.tif OUT-castle-10.tif -71"
+time ~/seamc castle-10.tif OUT-castle-10.tif -71
+echo "time ~/seamc castle-20.tif OUT-castle-20.tif -143"
+time ~/seamc castle-20.tif OUT-castle-20.tif -143
+echo "time ~/seamc castle-30.tif OUT-castle-30.tif -214"
+time ~/seamc castle-30.tif OUT-castle-30.tif -214
+echo "time ~/seamc castle-40.tif OUT-castle-40.tif -285"
+time ~/seamc castle-40.tif OUT-castle-40.tif -285
+echo "time ~/seamc castle-50.tif OUT-castle-50.tif -357"
+time ~/seamc castle-50.tif OUT-castle-50.tif -357
+echo "time ~/seamc castle-60.tif OUT-castle-60.tif -428"
+time ~/seamc castle-60.tif OUT-castle-60.tif -428
+echo "time ~/seamc castle-70.tif OUT-castle-70.tif -500"
+time ~/seamc castle-70.tif OUT-castle-70.tif -500
+echo "time ~/seamc castle-80.tif OUT-castle-80.tif -571"
+time ~/seamc castle-80.tif OUT-castle-80.tif -571
+echo "time ~/seamc castle-90.tif OUT-castle-90.tif -642"
+time ~/seamc castle-90.tif OUT-castle-90.tif -642
+echo "time ~/seamc castle-100.ti OUT-castle-100.ti -714"
+time ~/seamc castle-100.ti OUT-castle-100.ti -714
